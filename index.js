b8e07116a879d5d152596f0b1df425bae2e9b880
Wells Johnston <wellsjohnston@gmail.com>
2016-04-28 18:11:59 -0400

    update structure'

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict'
 
 const config = require('./config');
+const Match = require('./lib/match');
 const map = require('./maps/1.json');
 const io = require('./server');
+
+const games = [];
 
 io.on('connection', socket => {
 
   console.log('connection');
 
-  socket.on('ping', data => {
-    console.log('ping');
-    socket.emit('pong');
+  socket.on('action', action => {
+    socket.broadcast();
   });
-
 });
