3d50f662fba7994a5ce08e2df236e7142f97cc6c
Wells Johnston <wellsjohnston@gmail.com>
2016-04-29 11:00:47 -0400

    simple match making system in place

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict'
 
 const config = require('./config');
-const Match = require('./lib/match');
-const map = require('./maps/1.json');
+const Match = require('./lib/Match');
+const map = require('./maps/map-1.json');
 const io = require('./server');
 
-const games = [];
+// array of games in memory
+const matches = [];
 
+// array of players awaiting connection
+const waitroom = [];
+
+// waitroom / connection logic
 io.on('connection', socket => {
-
-  console.log('connection');
-
-  socket.on('action', action => {
-    socket.broadcast();
-  });
+  if (!waitroom.length) {
+    waitroom.push(socket);
+  } else {
+    let p1 = waitroom.pop();
+    let p2 = socket;
+    matches.push(new Match(p1, p2, matches.length));
+  }
 });
