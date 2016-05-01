733aba3236f3137da70e555a82b88faaadb4aa22
Wells Johnston <wellsjohnston@gmail.com>
2016-05-01 01:48:18 -0400

    tightened up the code a bit

--- index.js
+++ index.js
@@ -7,35 +7,30 @@
 const Match = require('./lib/Match');
 const io = require('./server');
 const gen = require('shortid').generate;
-
-// waitroom and matches
 const waitroom = [], matches = [];
-setInterval(_ => console.info(matches.map(m => m.id)), 10000);
 
 // on connection either start a match or enter a waitroom
 io.on('connection', socket => {
 
+  // give the socket (player) an id
   socket.id = gen();
-
-  socket.on('disconnect', _ => {
-    waitroom.filter(s => s.id = socket.id);
-  });
-
-  console.info('new connection');
 
   if (!waitroom.length) {
 
     // queue waitroom
     waitroom.push(socket);
     socket.emit('waiting');
-    console.info('connection waiting')
   } else {
 
     // start match
     let match = new Match(io, gen(), 'map-1', waitroom.pop(), socket);
     matches.push(match);
-    match.on('end', _ => {
-      console.log('match ended');
-    });
+    console.info(match);
   }
+
+  // if a user disconnects, remove them from the waitroom
+  socket.on('disconnect', _ => {
+    waitroom = waitroom.filter(s => s.id != socket.id);
+    matches = matches.filter(m => m.id != s.mid);
+  });
 });
